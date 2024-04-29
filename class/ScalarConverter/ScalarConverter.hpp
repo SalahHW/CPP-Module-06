6ee1d0c9420476cc544ab61f25f6fd38199eec1b
Salah <salah.bouheniche@gmail.com>
2024-04-30 00:44:18 +0200

    Fix typo

--- class/ScalarConverter/ScalarConverter.hpp
+++ class/ScalarConverter/ScalarConverter.hpp
@@ -6,20 +6,21 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:41:32 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/29 15:29:48 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/30 00:40:42 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALAR_CONVERTER_HPP
-#define SCALAR_CONERTER_HPP
+#define SCALAR_CONVERTER_HPP
 
 #include <string>
+#include <iostream>
 
 class ScalarConverter
 {
     private:
     public:
-        void convert(std::string const &);
+        static void convert(std::string const &);
 };
 
 #endif
