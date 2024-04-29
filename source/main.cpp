54fdc13e1bfda2f4570898e388214d7e1760702a
Salah <salah.bouheniche@gmail.com>
2024-04-30 00:44:44 +0200

    Update main for tests

--- source/main.cpp
+++ source/main.cpp
@@ -6,11 +6,19 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:39:58 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/22 18:40:12 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/29 16:17:03 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include <iostream>
+
+int main(int argc, char **argv)
 {
+    (void) argv;
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./convert [value]" << std::endl;
+        return (1);
+    }
     return (0);
 }
