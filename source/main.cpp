34299c46f795023fbc7d71f5ce83f6df5203c0b8
Salah <salah.bouheniche@gmail.com>
2024-04-30 00:48:03 +0200

    Update main.cpp to include ScalarConverter.hpp

--- source/main.cpp
+++ source/main.cpp
@@ -6,10 +6,11 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:39:58 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/29 16:17:03 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/30 00:47:11 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../class/ScalarConverter/ScalarConverter.hpp"
 #include <iostream>
 
 int main(int argc, char **argv)
@@ -20,5 +21,6 @@ int main(int argc, char **argv)
         std::cerr << "Usage: ./convert [value]" << std::endl;
         return (1);
     }
+    ScalarConverter::convert(argv[1]);
     return (0);
 }
