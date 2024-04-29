1d420bdd968188bee0aa269fc4b175a58d94f056
Salah <salah.bouheniche@gmail.com>
2024-04-29 15:32:52 +0200

    Add ScalarConverter::convert method implementation

--- class/ScalarConverter/ScalarConverter.cpp
+++ class/ScalarConverter/ScalarConverter.cpp
@@ -6,8 +6,13 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:40:53 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/22 18:41:45 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:31:52 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+
+void ScalarConverter::convert(std::string const &input)
+{
+    
+}
--- class/ScalarConverter/ScalarConverter.hpp
+++ class/ScalarConverter/ScalarConverter.hpp
@@ -6,16 +6,20 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:41:32 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/29 15:26:55 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:29:48 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALAR_CONVERTER_HPP
 #define SCALAR_CONERTER_HPP
 
+#include <string>
+
 class ScalarConverter
 {
-    
+    private:
+    public:
+        void convert(std::string const &);
 };
 
 #endif
