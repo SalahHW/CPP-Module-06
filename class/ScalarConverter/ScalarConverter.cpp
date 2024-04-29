8931c15f249e7900ab96b812b9a6ed43e29c3b85
Salah <salah.bouheniche@gmail.com>
2024-04-30 00:43:48 +0200

    Add output message for debugging purposes

--- class/ScalarConverter/ScalarConverter.cpp
+++ class/ScalarConverter/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:40:53 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/29 15:31:52 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:41:42 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void ScalarConverter::convert(std::string const &input)
 {
-    
+    std::cout << "Convert called on " << input << std::endl;
 }
