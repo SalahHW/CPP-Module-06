5c05450c57590cea28466821b2d8b50b59a45ccd
Salah <salah.bouheniche@gmail.com>
2024-04-29 15:28:19 +0200

    Add preprocessor directives

--- class/ScalarConverter/ScalarConverter.hpp
+++ class/ScalarConverter/ScalarConverter.hpp
@@ -6,11 +6,16 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:41:32 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/04/22 18:42:11 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:26:55 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+#define SCALAR_CONERTER_HPP
 
 class ScalarConverter
 {
     
 };
+
+#endif
