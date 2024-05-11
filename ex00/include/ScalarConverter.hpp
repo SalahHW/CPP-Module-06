9257eec1106b2653021f415b1038dff1b42bf7cb
Salah <salah.bouheniche@gmail.com>
2024-05-11 04:04:01 +0200

    Refactor project

---
+++ ex00/include/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+# include <iostream>
+# include <string>
+
+class ScalarConverter
+{
+  private:
+  public:
+	static void convert(std::string const &);
+};
+
+#endif
---
+++ ex00/source/ScalarConverter.cpp
@@ -0,0 +1,8 @@
+#include "ScalarConverter.hpp"
+
+void ScalarConverter::convert(std::string const &input)
+{
+
+    std::cout << "Convert called on " << input << std::endl;
+    // Detect type
+}
